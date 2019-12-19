bd3d9de58bcc8915c8ba55049b01523e00a48949
Haoming.Ju <JuHaoming@Trunk.tech>
2019-12-19 11:10:36 +0800

    Sort the key of conanfile.py file

--- conanfile.py
+++ conanfile.py
@@ -4,12 +4,12 @@ from conans import ConanFile, tools
 class GlogConan(ConanFile):
     name = "gflag"  # TODO: The package name
     version = "2.2.2"  # TODO: The package version
-    settings = "os", "compiler", "build_type", "arch"
     description = "<Description of Glog here>"
-    url = "None"
-    license = "MIT"
     author = "Trunk"  # TODO: The author
+
+    license = "MIT"
     topics = None
+    settings = "os", "compiler", "build_type", "arch"
     exports_sources = name + "/*"  # XXX: The upload path
 
     def package(self):
